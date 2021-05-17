cf2e4995ef48636ed7bcf4a097da9b81fecebbf4
Sander Stad <sanderstad@gmail.com>
2021-05-17 11:11:19 +0200

    Added excludes to avoid errors running pester

--- templates/PSFTests/general/PSScriptAnalyzer.Tests.ps1
+++ templates/PSFTests/general/PSScriptAnalyzer.Tests.ps1
@@ -2,7 +2,7 @@
 Param (
 	[switch]
 	$SkipTest,
-	
+
 	[string[]]
 	$CommandPath = @("$global:testroot\..\functions", "$global:testroot\..\internal\functions")
 )
@@ -14,23 +14,21 @@ $global:__pester_data.ScriptAnalyzer = New-Object System.Collections.ArrayList
 Describe 'Invoking PSScriptAnalyzer against commandbase' {
 	$commandFiles = Get-ChildItem -Path $CommandPath -Recurse | Where-Object Name -like "*.ps1"
 	$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-	
-	foreach ($file in $commandFiles)
-	{
+
+	foreach ($file in $commandFiles) {
 		Context "Analyzing $($file.BaseName)" {
-			$analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule PSAvoidTrailingWhitespace, PSShouldProcess
-			
-			forEach ($rule in $scriptAnalyzerRules)
-			{
+			[array]$excludes = 'PSAvoidUsingInvokeExpression', 'PSAvoidTrailingWhitespace', 'PSShouldProcess', 'PSAvoidGlobalVars'
+
+			$analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule $excludes
+
+			forEach ($rule in $scriptAnalyzerRules) {
 				It "Should pass $rule" -TestCases @{ analysis = $analysis; rule = $rule } {
-					If ($analysis.RuleName -contains $rule)
-					{
+					If ($analysis.RuleName -contains $rule) {
 						$analysis | Where-Object RuleName -EQ $rule -outvariable failures | ForEach-Object { $null = $global:__pester_data.ScriptAnalyzer.Add($_) }
-						
+
 						1 | Should -Be 0
 					}
-					else
-					{
+					else {
 						0 | Should -Be 0
 					}
 				}
